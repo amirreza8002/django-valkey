bb19f350a1ab84ea10446d8d72dbf5a7d224fdec
amirreza <amir.rsf1380@gmail.com>
2024-09-03 14:37:58 +0330

    fixed text

--- django_valkey/exceptions.py
+++ django_valkey/exceptions.py
@@ -5,7 +5,7 @@ class ConnectionInterrupted(Exception):
     def __str__(self) -> str:
         error_type = type(self.__cause__).__name__
         error_msg = str(self.__cause__)
-        return f"Redis {error_type}: {error_msg}"
+        return f"Valkey {error_type}: {error_msg}"
 
 
 class CompressorError(Exception):
