d3a72cb1a5ec170a061a10d0df60f6958c3dc997
amirreza <amir.rsf1380@gmail.com>
2024-09-08 20:24:19 +0330

    i don't know why they still existed

--- tests/settings/sqlite_sharding.py
+++ tests/settings/sqlite_sharding.py
@@ -3,17 +3,17 @@ SECRET_KEY = "django_tests_secret_key"
 CACHES = {
     "default": {
         "BACKEND": "django_valkey.cache.ValkeyCache",
-        "LOCATION": ["valkey://127.0.0.1:6379?db=1", "redis://127.0.0.1:6379?db=2"],
+        "LOCATION": ["valkey://127.0.0.1:6379?db=1", "valkey://127.0.0.1:6379?db=2"],
         "OPTIONS": {"CLIENT_CLASS": "django_valkey.client.ShardClient"},
     },
     "doesnotexist": {
         "BACKEND": "django_valkey.cache.ValkeyCache",
-        "LOCATION": ["valkey://127.0.0.1:56379?db=1", "redis://127.0.0.1:56379?db=2"],
+        "LOCATION": ["valkey://127.0.0.1:56379?db=1", "valkey://127.0.0.1:56379?db=2"],
         "OPTIONS": {"CLIENT_CLASS": "django_valkey.client.ShardClient"},
     },
     "sample": {
         "BACKEND": "django_valkey.cache.ValkeyCache",
-        "LOCATION": "valkey://127.0.0.1:6379?db=1,redis://127.0.0.1:6379?db=1",
+        "LOCATION": "valkey://127.0.0.1:6379?db=1,valkey://127.0.0.1:6379?db=1",
         "OPTIONS": {"CLIENT_CLASS": "django_valkey.client.ShardClient"},
     },
     "with_prefix": {
--- tests/settings/sqlite_zlib.py
+++ tests/settings/sqlite_zlib.py
@@ -3,7 +3,7 @@ SECRET_KEY = "django_tests_secret_key"
 CACHES = {
     "default": {
         "BACKEND": "django_valkey.cache.ValkeyCache",
-        "LOCATION": ["valkey://127.0.0.1:6379?db=1", "redis://127.0.0.1:6379?db=1"],
+        "LOCATION": ["valkey://127.0.0.1:6379?db=1", "valkey://127.0.0.1:6379?db=1"],
         "OPTIONS": {
             "CLIENT_CLASS": "django_valkey.client.DefaultClient",
             "COMPRESSOR": "django_valkey.compressors.zlib.ZlibCompressor",
@@ -19,7 +19,7 @@ CACHES = {
     },
     "sample": {
         "BACKEND": "django_valkey.cache.ValkeyCache",
-        "LOCATION": "valkey://127.0.0.1:6379?db=1,redis://127.0.0.1:6379?db=1",
+        "LOCATION": "valkey://127.0.0.1:6379?db=1,valkey://127.0.0.1:6379?db=1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_valkey.client.DefaultClient",
             "COMPRESSOR": "django_valkey.compressors.zlib.ZlibCompressor",
