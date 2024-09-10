6dc944b83061724ad55209e4fa92badcecca97d5
amirreza <amir.rsf1380@gmail.com>
2024-09-10 18:27:24 +0330

    add awaitable check

--- django_valkey/__init__.py
+++ django_valkey/__init__.py
@@ -1,3 +1,5 @@
+from inspect import isawaitable
+
 VERSION = (0, 0, 16)
 __version__ = ".".join(map(str, VERSION))
 
@@ -17,5 +19,8 @@ def get_valkey_connection(alias="default", write=True):
 
     if not hasattr(cache.client, "get_client"):
         raise NotImplementedError(error_message)
+
+    if isawaitable(cache.client.get_client):
+        raise "use django_valkey.async_cache.get_valkey_connection for async backends"
 
     return cache.client.get_client(write)
