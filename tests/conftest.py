f393e762b49c0c60bd7d5b8d8e5f3a8ff57f8af9
amirreza <amir.rsf1380@gmail.com>
2024-09-15 07:41:53 +0330

    fixed a type hint

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ from typing import Iterable
 import pytest
 from django.core.cache import cache as default_cache
 
-from django_valkey.cache import BaseCache
+from django_valkey.cache import BaseValkeyCache
 
 
 @pytest.fixture
-def cache() -> Iterable[BaseCache]:
+def cache() -> Iterable[BaseValkeyCache]:
     yield default_cache
     default_cache.clear()
