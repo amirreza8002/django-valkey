1eeb0c65e696b79883727bdc5bd0ec837817e015
amirreza <amir.rsf1380@gmail.com>
2024-10-10 17:32:24 +0330

    adjust test suite with new format

--- tests/tests_async/test_connection_string.py
+++ tests/tests_async/test_connection_string.py
@@ -14,7 +14,9 @@ from django_valkey import pool
 @pytest.mark.asyncio
 async def test_connection_strings(connection_string: str):
     cf = pool.get_connection_factory(
-        path="django_valkey.async_cache.pool.AsyncConnectionFactory", options={}
+        options={
+            "CONNECTION_FACTORY": "django_valkey.async_cache.pool.AsyncConnectionFactory"
+        }
     )
     res = cf.make_connection_params(connection_string)
     assert res["url"] == connection_string
