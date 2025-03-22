1675272a2b63a883141705a51e910709771c1800
amirreza <amir.rsf1380@gmail.com>
2025-03-22 20:34:27 +0330

    fix conftest
    (cherry picked from commit d924df5e39341690afcc36033e74674e28ee9928)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,22 @@ from collections.abc import Iterable
 
 import pytest
 import pytest_asyncio
+
+from asgiref.compatibility import iscoroutinefunction
 from django.core.cache import cache as default_cache
 
 from django_valkey.base import BaseValkeyCache
 
 
 # for some reason `isawaitable` doesn't work here
-try:
+if iscoroutinefunction(default_cache.clear):
 
     @pytest_asyncio.fixture(loop_scope="session")
     async def cache():
         yield default_cache
         await default_cache.aclear()
 
-except Exception:
+else:
 
     @pytest.fixture
     def cache() -> Iterable[BaseValkeyCache]:
