cea7cf64e8255bd80a89133220d1bc1dc507003b
amirreza <amir.rsf1380@gmail.com>
2025-04-11 05:09:56 +0330

    remove commented-out codes that won't be needed anymore
    (cherry picked from commit 4c6d8f459178509c38344b3f2a9e38292415263c)

--- tests/tests_async/test_cache_options.py
+++ tests/tests_async/test_cache_options.py
@@ -105,9 +105,6 @@ class TestDjangoValkeyCacheEscapePrefix:
     async def test_iter_keys(
         self, key_prefix_cache: AsyncValkeyCache, with_prefix_cache: AsyncValkeyCache
     ):
-        # if isinstance(key_prefix_cache.client, ShardClient):
-        #     pytest.skip("ShardClient doesn't support iter_keys")
-
         await key_prefix_cache.aset("a", "1")
         await with_prefix_cache.aset("b", "2")
         async with contextlib.aclosing(key_prefix_cache.aiter_keys("*")) as keys:
@@ -133,9 +130,6 @@ async def test_custom_key_function(cache: AsyncValkeyCache, settings: SettingsWr
         "REVERSE_KEY_FUNCTION"
     ] = "tests.test_cache_options.reverse_key"
     settings.CACHES = caches_setting
-
-    # if isinstance(cache.client, ShardClient):
-    #     pytest.skip("ShardClient doesn't support get_client")
 
     for key in ["foo-aa", "foo-ab", "foo-bb", "foo-bc"]:
         await cache.aset(key, "foo")
