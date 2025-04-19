ff32a17968f3f73cd11c4045a9e096f564e93546
amirreza <amir.rsf1380@gmail.com>
2025-04-19 19:52:57 +0330

    move key/value related methods to a function
    this was done so people can use them with a raw client
    
    (cherry picked from commit d39a5ab9ea0d72a5511f09511de982052ebe4ab8)

--- django_valkey/util.py
+++ django_valkey/util.py
@@ -1,3 +1,14 @@
+import re
+from contextlib import suppress
+from typing import Any
+
+from valkey.typing import KeyT, EncodableT
+
+from django_valkey.compressors.base import BaseCompressor
+from django_valkey.exceptions import CompressorError
+from django_valkey.serializers.base import BaseSerializer
+
+
 class CacheKey(str):
     """
     A stub string class that we can use to check if a key was created already.
@@ -9,3 +20,56 @@ class CacheKey(str):
 
 def default_reverse_key(key: str) -> str:
     return key.split(":", 2)[2]
+
+
+special_re = re.compile("([*?[])")
+
+
+def glob_escape(s: str) -> str:
+    return special_re.sub(r"[\1]", s)
+
+
+def make_key(
+    key: KeyT, key_func, version: int | None = None, prefix: str | None = None
+) -> KeyT:
+    if isinstance(key, CacheKey):
+        return key
+
+    return CacheKey(key_func(key, prefix, version))
+
+
+def make_pattern(
+    pattern: str, key_func, version: int | None = None, prefix: str | None = None
+) -> KeyT:
+    if isinstance(pattern, CacheKey):
+        return pattern
+
+    prefix = glob_escape(prefix)
+    version_str = glob_escape(str(version))
+
+    return CacheKey(key_func(pattern, prefix, version_str))
+
+
+def encode(
+    value: EncodableT, serializer: BaseSerializer, compressor: BaseCompressor
+) -> bytes | int | float:
+    if type(value) not in {int, float}:
+        value = serializer.dumps(value)
+        return compressor.compress(value)
+
+    return value
+
+
+def decode(value: bytes, serializer: BaseSerializer, compressor: BaseCompressor) -> Any:
+    try:
+        if value.isdigit():
+            value = int(value)
+        else:
+            value = float(value)
+
+    except (ValueError, TypeError):
+        with suppress(CompressorError):
+            value = compressor.decompress(value)
+        value = serializer.loads(value)
+
+    return value
