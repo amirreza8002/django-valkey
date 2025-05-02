9a911886c4bd6a20b69a9a146740a80b9d2cdd92
amirreza <amir.rsf1380@gmail.com>
2025-05-02 04:12:36 +0330

    add hgetall hash method

--- django_valkey/base.py
+++ django_valkey/base.py
@@ -334,6 +334,9 @@ class BackendCommands:
     def hget(self: BaseValkeyCache, *args, **kwargs) -> Any:
         return self.client.hget(*args, **kwargs)
 
+    def hgetall(self: BaseValkeyCache, *args, **kwargs) -> dict[str, Any]:
+        return self.client.hgetall(*args, **kwargs)
+
     def hkeys(self: BaseValkeyCache, *args, **kwargs) -> list[Any]:
         return self.client.hkeys(*args, **kwargs)
 
@@ -527,6 +530,9 @@ class AsyncBackendCommands:
 
     async def hget(self, *args, **kwargs) -> Any:
         return await self.client.hget(*args, **kwargs)
+
+    async def hgetall(self, *args, **kwargs) -> dict[str, Any]:
+        return await self.client.hgetall(*args, **kwargs)
 
     async def hkeys(self, *args, **kwargs) -> list[Any]:
         return await self.client.hkeys(*args, **kwargs)
