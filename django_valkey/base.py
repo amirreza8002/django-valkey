18ff9a2ffd7542d71270ca7707dd685c6baae525
amirreza <amir.rsf1380@gmail.com>
2025-05-06 01:54:05 +0330

    move contains to base command class so cluster can reuse it

--- django_valkey/base.py
+++ django_valkey/base.py
@@ -160,6 +160,9 @@ class BaseValkeyCache(Generic[Client, Backend]):
 
 @decorate_all_methods(omit_exception)
 class BackendCommands:
+    def __contains__(self, item):
+        return self.has_key(item)
+
     def set(self: BaseValkeyCache, *args, **kwargs) -> bool:
         return self.client.set(*args, **kwargs)
 
